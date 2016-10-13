260ee7d3451b010d2544e645e278c38c2e0c7b3e
Adhitya Ramadhanus <adhitya.ramadhanus@gmail.com>
2016-10-14 00:04:17 +0700

    fix code style

--- filter.go
+++ filter.go
@@ -1,15 +1,10 @@
 package gofp
 
-import (
-	"fmt"
-)
-
 func Filter(filter func(xi interface{}) bool, x []interface{}) []interface{} {
 	result := make([]interface{}, 0, len(x))
-	for i := 0; i < len(x); i++ {
-		// fmt.Println(filter(x[i]))
-		if filter(x[i]) {
-			result = append(result, x[i])
+	for _, v := range x {
+		if filter(v) {
+			result = append(result, v)
 		}
 	}
 	return result
--- map.go
+++ map.go
@@ -1,14 +1,9 @@
 package gofp
 
-import (
-	"fmt"
-)
-
 func Map(mapper func(xi interface{}) interface{}, x []interface{}) []interface{} {
 	result := make([]interface{}, len(x), len(x))
-	for i := 0; i < len(x); i++ {
-		// fmt.Println(mapper(x[i]))
-		result[i] = mapper(x[i])
+	for i, v := range x {
+		result[i] = mapper(v)
 	}
 	return result
 }
